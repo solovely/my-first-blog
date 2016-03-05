f44629f58f0b54d922ac265acd72af0650f902dc
solovely <postma.lydia@gmail.com>
2016-03-05 11:37:41 +0100

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = 'Europe/Amsterdam'
 
 USE_I18N = True
 
@@ -101,4 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
